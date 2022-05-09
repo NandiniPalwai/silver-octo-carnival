3f67f18b785f1c1ecfa89ed94766651ca5d9ed98
Nandini Palvai <nandinipalvai54@gmail.com>
2022-05-08 21:57:46 -0400

    chore: initial commit

---
+++ src/main/java/com/flight_analysis/App.java
@@ -0,0 +1,12 @@
+package com.flight_analysis;
+
+/**
+ * Hello world!
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World2!" );
+    }
+}
